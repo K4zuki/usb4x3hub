a2544122392f94746f97772e16449a8dce78cc86
K4zuki <k.yamamoto.08136891@gmail.com>
2015-11-09 16:18:12 +0900

    .

---
+++ usb4x3hub/main.cpp
@@ -0,0 +1,69 @@
+
+#include <mbed.h>
+
+DigitalOut OE(P0_13);
+BusOut SEL(P0_6,P0_7);
+InterruptIn SW1(P0_3);
+InterruptIn SW2(P0_2);
+InterruptIn SW3(P0_11);
+InterruptIn SW4(P0_10);
+
+// Setup the Serial to the PC for debugging
+Serial pc(P0_4, P0_0);
+
+
+void SW1_int()
+{
+    OE = 1;
+    SEL = 0;
+    OE = 0;
+    pc.printf("SW1\n\r");
+}
+
+void SW2_int()
+{
+    OE = 1;
+    SEL = 1;
+    OE = 0;
+    pc.printf("SW2\n\r");
+}
+
+void SW3_int()
+{
+    OE = 1;
+    SEL = 2;
+    OE = 0;
+    pc.printf("SW3\n\r");
+}
+
+void SW4_int()
+{
+    OE = 1;
+    SEL = 3;
+    OE = 0;
+    pc.printf("SW4\n\r");
+}
+
+int main()
+{
+
+    pc.baud(57600);
+    pc.printf("\nHello from USB4x3HUB \n\r");
+
+    SEL = 0;
+    OE = 1;
+
+    SW1.fall(&SW1_int);
+    SW1.mode(PullUp);
+    SW2.fall(&SW2_int);
+    SW2.mode(PullUp);
+    SW3.fall(&SW3_int);
+    SW3.mode(PullUp);
+    SW4.fall(&SW4_int);
+    SW4.mode(PullUp);
+
+    while (1) {
+        wait(5);
+        pc.printf(".");
+    }
+}
